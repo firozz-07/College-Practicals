41857d5cbe8b98ecf190c2ace450f6a5e2df693d
firoz <firozz0007@gmail.com>
2026-07-19 00:14:57 +0530

    String Buffer

---
+++ Java/Palindrome2.java
@@ -0,0 +1,13 @@
+public class Palindrome2 {
+    public static void main(String[] args) {
+        StringBuffer str=new StringBuffer("level");
+        StringBuffer rev=new StringBuffer(str);
+        str.reverse();
+        if(rev.toString().equals(str.toString())){
+            System.out.println("the string is palindrome");
+        }
+        else{
+            System.out.println("the string is not  palindrome");
+        }
+    }
+}
--- Java/cmd.java
+++ Java/cmd.java
@@ -1,4 +1,5 @@
-package Java;public class cmd {
+package Java;
+public class cmd {
     public static void main(String args[]) {
         int sum=0;
         for (int i = 0; i < args.length; i++) {
