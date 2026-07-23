b96bd0640b8ead99a4d17d483f68d5699c3d801d
firoz <firozz0007@gmail.com>
2026-07-23 22:54:56 +0530

    tomorrows practical

---
+++ VSC1/Java/ReplaceString.java
@@ -0,0 +1,7 @@
+public class ReplaceString {
+    public static void main(String[] args) {
+        String str="IiiiiiAMiiiFIROZii";
+        str=str.replaceAll("i"," ");
+        System.out.println(str);
+    }
+}
