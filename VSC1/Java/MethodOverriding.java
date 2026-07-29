3ac6f04421f89b9f3436f84d688d65cdbf872c57
firoz <firozz0007@gmail.com>
2026-07-29 19:04:14 +0530

    tomorrows practical

---
+++ VSC1/Java/MethodOverriding.java
@@ -0,0 +1,19 @@
+ class cat{
+    public void sound(){
+        System.out.println("A cat meowwwws");
+    }
+}
+ class dog extends cat {
+    @Override
+    public void sound(){
+        System.out.println("A dog barksss");
+    }
+}
+public class MethodOverriding{
+    public static void main(String[] args) {
+        cat cats=new cat();
+        dog dogs=new dog();
+        cats.sound();
+        dogs.sound();
+    }
+}
