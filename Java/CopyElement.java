b86b3dc3e325da8fcbf97a71f4bfe4dcaac2f05b
firoz <firozz0007@gmail.com>
2026-07-29 19:27:58 +0530

    tomorrows practical

--- Java/CopyElement.java
+++ Java/CopyElement.java
@@ -2,9 +2,7 @@ public class CopyElement {
     public static void main(String[] args) {
         int [] arr={2,3,4,5,6};
         int [] arr2=new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr2[i] = arr[i];
-        }
-        for(int k:arr2) System.out.print(k+" ");
+        for (int i = 0; i < arr.length; i++) {arr2[i] = arr[i];}
+        for(int i:arr2) System.out.print(i+" ");
     }
 }
