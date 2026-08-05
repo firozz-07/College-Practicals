c92fc1c1b0e9691ef55bda971107c36cf75e6a54
firoz <firozz0007@gmail.com>
2026-08-05 09:54:40 +0530

    Today's practical

---
+++ Python/FilterMapReduce.py
@@ -0,0 +1,11 @@
+
+from functools import reduce 
+number=[12,31,90,67]
+odd=list(filter(lambda x:x%2==1,number))
+print(" filter's Example : ",odd)
+
+double=list(map(lambda x:x*2,number))
+print("Map example : ", double)
+
+total=reduce(lambda x,y:x+y,number)
+print("Reduce Example :",total)
