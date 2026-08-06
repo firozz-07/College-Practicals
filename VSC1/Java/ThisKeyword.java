384b0b9aabd8f89ca7573368ea1076af61efef22
firoz <firozz0007@gmail.com>
2026-08-06 20:07:42 +0530

    peaseful practicals

--- VSC1/Java/ThisKeyword.java
+++ VSC1/Java/ThisKeyword.java
@@ -1,7 +1,24 @@
+class about {
+    public String name;
+    public String surname;
 
+    about(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
-public class ThisKeyword {
+    public void displayName() {
+        System.out.println("my name is "+ this.name);
+    }
+
+    public void displaySurname() {
+        System.out.println( "my surname is "+ this.surname);
+    }
+}
+public class ThisKeyword{
     public static void main(String[] args) {
-        
+        about obj=new about("Firoz", "Pathan");
+        obj.displayName();
+        obj.displaySurname();
     }
 }
