66917564d8dcefde7cd36d7b5c186fd016b98057
firoz <firozz0007@gmail.com>
2026-08-06 20:13:18 +0530

    peaceful practicals

---
+++ VSC1/Java/Constructor.java
@@ -0,0 +1,18 @@
+class sum{
+   public int a;
+   public int b;
+   sum(int a,int b){
+      this.a=a;
+      this.b=b;
+   }
+   public void displaySum(){
+      System.out.println("Addition is : "+(this.a+this.b));
+   }
+}
+
+public class Constructor {
+   public static void main(String[] args) {
+    sum obj=new sum(10, 20);
+    obj.displaySum();
+   } 
+}
