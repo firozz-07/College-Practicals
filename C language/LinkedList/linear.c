3cf6586d108fdaffd3678f5e48f99584539631d4
firoz <firozz0007@gmail.com>
2026-09-19 10:47:22 +0530

    practicing c

--- C language/LinkedList/linear.c
+++ C language/LinkedList/linear.c
@@ -57,3 +57,74 @@ insertpos()
     temp->next = newnode;
   }
 }
+void deletebeg(){
+  struct node *temp;
+  if(head==NULL)printf("empty");
+  else{
+    temp=head;
+    head=temp->next;
+    free(temp);
+  }
+}
+void deleteend(){
+  struct node *temp;
+  struct node *del;
+    if(head==NULL)printf("empty");
+    if(head->next==NULL) {
+      free(head);
+    }
+    temp = head;
+
+    while (temp->next->next != NULL)
+        temp = temp->next;
+
+    del = temp->next;
+    temp->next = NULL;
+    free(del);
+}
+void deletepos(){
+  int pos, i;
+    struct node *temp, *del;
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    if (pos == 1) {
+        deleteBeg();
+        return;
+    }
+
+    temp = head;
+
+    for (i = 1; i < pos - 1 && temp->next != NULL; i++)
+        temp = temp->next;
+
+    if (temp->next == NULL) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    del = temp->next;
+    temp->next = del->next;
+    free(del);
+}
+void display(){
+  struct node *temp = head;
+
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
